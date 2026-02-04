3501a7c535622d1953ebdd04e464b01784179661
Amber <amberzeng@gmail.com>
2026-02-04 18:32:12 -0500

    fixed config?

--- cloudflare.js
+++ cloudflare.js
@@ -1,41 +1,18 @@
 export default {
     async fetch(request, env) {
-      const corsHeaders = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
-        'Access-Control-Max-Age': '86400',
-      };
-  
-      // Check for preflight request from the browser.
-      if (request.method === 'OPTIONS') {
-        return new Response(null, {
-          headers: {
-            ...corsHeaders,
-            'Access-Control-Allow-Headers': request.headers.get(
-              'Access-Control-Request-Headers'
-            ),
-          }
-        });
-      } else {
-        // Handle actual request and store image to bucket.
-        const { headers } = request;
-        
-        // Key is date now since we want keys to be unique. 
-        const key = Date.now();
-        await env.MY_BUCKET.put(key, request.body, {
-          httpMetadata: {
-            contentType: headers.get('content-type')
-          }
-        });
-  
-        return new Response('success!', {
-          headers: {
-            ...corsHeaders,
-            'Access-Control-Allow-Headers': request.headers.get(
-              'Access-Control-Request-Headers'
-            ),
-          }
-        });
+      const url = new URL(request.url);
+      const key = url.pathname.slice(1);
+      switch (request.method) {
+        case "PUT":
+          await env.CREATURA_BUCKET.put(key, request.body);
+          return new Response(`Put ${key} successfully!`);
+        default:
+          return new Response(`${request.method} is not allowed.`, {
+            status: 405,
+            headers: {
+              Allow: "PUT",
+            },
+          });
       }
-    }
-  }
+    },
+  };
